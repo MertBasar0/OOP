3d42e9b51457e99c09be50cfd657befec512ecfd
Mert <mertbasar0@hotmail.com>
2022-03-30 14:36:13 +0300

    30.03.2022-2

---
+++ PiggyBank/Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyBank
+{
+    public static class Consts
+    {
+        public const decimal Pi = 3.14159M;
+    }
+}
